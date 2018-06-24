8a3f46d9e473c86396a096ff6059d0faf3c6ba14
mfouda214 <m.fouda@robone.net>
2018-06-24 23:54:34 +0100

    Prepare for Relationships

---
+++ ShoppingList/AddListViewController.swift
@@ -0,0 +1,159 @@
+//
+//  AddListViewController.swift
+//  ShoppingList
+//
+//  Created by Mohamed Sobhi  Fouda on 6/24/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+import SVProgressHUD
+
+protocol AddListViewControllerDelegate {
+    func controller(controller: AddListViewController, didAddList list: CKRecord)
+    func controller(controller: AddListViewController, didUpdateList list: CKRecord)
+}
+
+class AddListViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    var delegate: AddListViewControllerDelegate?
+    var newList: Bool = true
+    
+    var list: CKRecord?
+    
+    @IBAction func cancel(sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func save(sender: AnyObject) {
+    
+        // Helpers
+        let name = self.nameTextField.text! as NSString
+        
+        // Fetch Private Database
+        let privateDatabase = CKContainer.default().privateCloudDatabase
+        
+        if list == nil {
+            list = CKRecord(recordType: "Lists")
+        }
+        
+        // Configure Record
+        list?.setObject(name, forKey: "name")
+        
+        // Show Progress HUD
+        SVProgressHUD.show()
+        
+        // Save Record
+        privateDatabase.save(list!) { (record, error) -> Void in
+            DispatchQueue.main.sync {
+                // Dismiss Progress HUD
+                SVProgressHUD.dismiss()
+                
+                // Process Response
+                self.processResponse(record: record, error: error)
+            }
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupView()
+        
+        // Update Helper
+        self.newList = self.list == nil
+        
+        // Add Observer
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(AddListViewController.textFieldTextDidChange(notification:)), name: NSNotification.Name.UITextFieldTextDidChange, object: nameTextField)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        nameTextField.becomeFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func setupView() {
+        updateNameTextField()
+        updateSaveButton()
+    }
+    
+    // MARK: -
+    private func updateNameTextField() {
+        if let name = list?.object(forKey: "name") as? String {
+            nameTextField.text = name
+        }
+    }
+    
+    // MARK: -
+    private func updateSaveButton() {
+        let text = nameTextField.text
+        
+        if let name = text {
+            saveButton.isEnabled = !name.isEmpty
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
+    
+    // MARK: -
+    // MARK: Helper Methods
+    private func processResponse(record: CKRecord?, error: Error?) {
+        var message = ""
+        
+        if let error = error {
+            print(error)
+            message = "We were not able to save your list."
+            
+        } else if record == nil {
+            message = "We were not able to save your list."
+        }
+        
+        if !message.isEmpty {
+            // Initialize Alert Controller
+            let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            
+            // Present Alert Controller
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            // Notify Delegate
+            if newList {
+                delegate?.controller(controller: self, didAddList: list!)
+            } else {
+                delegate?.controller(controller: self, didUpdateList: list!)
+            }
+            
+            // Pop View Controller
+            self.dismiss(animated: true, completion: nil)
+            
+        }
+    }
+    
+    // MARK: -
+    // MARK: Notification Handling
+    @objc func textFieldTextDidChange(notification: NSNotification) {
+        updateSaveButton()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ShoppingList/ListsViewController.swift
@@ -0,0 +1,300 @@
+//
+//  ListsViewController.swift
+//  ShoppingList
+//
+//  Created by Mohamed Sobhi  Fouda on 6/24/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+import SVProgressHUD
+
+class ListsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddListViewControllerDelegate {
+    
+    // MARK: -
+    // MARK: Add List View Controller Delegate Methods
+    func controller(controller: AddListViewController, didAddList list: CKRecord) {
+        // Add List to Lists
+        lists.append(list)
+        
+        // Sort Lists
+        sortLists()
+        
+        // Update Table View
+        tableView.reloadData()
+        
+        // Update View
+        updateView()
+    }
+    
+    func controller(controller: AddListViewController, didUpdateList list: CKRecord) {
+        // Sort Lists
+        sortLists()
+        
+        // Update Table View
+        tableView.reloadData()
+    }
+    
+    static let ListCell = "ListCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    var lists = [CKRecord]()
+    
+    var selection: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupView()
+        fetchLists()
+        
+        // Fetch User Record ID
+//        fetchUserRecordID()
+        
+    }
+    
+    // MARK: View Methods
+    private func setupView() {
+        tableView.isHidden = true
+        messageLabel.isHidden = true
+        activityIndicatorView.startAnimating()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - FetchUserRecordID
+    private func fetchUserRecordID() {
+        // Fetch Default Container
+        let defaultContainer = CKContainer.default()
+        
+        // Fetch User Record
+        defaultContainer.fetchUserRecordID { (recordID, error) -> Void in
+            if let responseError = error {
+                print(responseError)
+                
+            } else if let userRecordID = recordID {
+                DispatchQueue.main.sync {
+                    self.fetchUserRecord(recordID: userRecordID)
+                }
+            }
+        }
+    }
+    
+    
+    private func fetchUserRecord(recordID: CKRecordID) {
+        // Fetch Default Container
+        let defaultContainer = CKContainer.default()
+        
+        // Fetch Private Database
+        let privateDatabase = defaultContainer.privateCloudDatabase
+        
+        // Fetch User Record
+        privateDatabase.fetch(withRecordID: recordID) { (record, error) -> Void in
+            if let responseError = error {
+                print(responseError)
+                
+            } else if let userRecord = record {
+                print(userRecord)
+            }
+        }
+    }
+    
+    private func fetchLists() {
+        // Fetch Private Database
+        let privateDatabase = CKContainer.default().privateCloudDatabase
+        
+        // Initialize Query
+        let query = CKQuery(recordType: "Lists", predicate: NSPredicate(value: true))
+        
+        // Configure Query
+        query.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        // Perform Query
+        privateDatabase.perform(query, inZoneWith: nil) { (records, error) in
+            records?.forEach({ (record) in
+                
+                guard error == nil else{
+                    print(error?.localizedDescription as Any)
+                    return
+                }
+                
+                print(record.value(forKey: "name") ?? "")
+                self.lists.append(record)
+                DispatchQueue.main.sync {
+                    self.tableView.reloadData()
+                    self.messageLabel.text = ""
+                    self.updateView()
+                }
+            })
+            
+        }
+    }
+    
+    private func updateView(){
+        let hasRecords = self.lists.count > 0
+        
+        self.tableView.isHidden = !hasRecords
+        messageLabel.isHidden = hasRecords
+        activityIndicatorView.stopAnimating()
+    }
+    
+    private func sortLists() {
+        self.lists.sort {
+            var result = false
+            let name0 = $0.object(forKey: "name") as? String
+            let name1 = $1.object(forKey: "name") as? String
+            
+            if let listName0 = name0, let listName1 = name1 {
+                result = listName0.localizedCaseInsensitiveCompare(listName1) == .orderedAscending
+            }
+            
+            return result
+        }
+    }
+    
+}
+
+// MARK: UITableView Delegate Methods
+extension ListsViewController{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue Reusable Cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: ListsViewController.ListCell, for: indexPath)
+        
+        // Configure Cell
+        cell.accessoryType = .detailDisclosureButton
+        
+        // Fetch Record
+        let list = lists[indexPath.row]
+        
+        if let listName = list.object(forKey: "name") as? String {
+            // Configure Cell
+            cell.textLabel?.text = listName
+            
+        } else {
+            cell.textLabel?.text = "-"
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {    guard editingStyle == .delete else { return }
+        
+        // Fetch Record
+        let list = lists[indexPath.row]
+        
+        // Delete Record
+        deleteRecord(list)
+    }
+    
+    private func deleteRecord(_ list: CKRecord) {
+        // Fetch Private Database
+        let privateDatabase = CKContainer.default().privateCloudDatabase
+        
+        // Show Progress HUD
+        SVProgressHUD.show()
+        
+        // Delete List
+        privateDatabase.delete(withRecordID: list.recordID) { (recordID, error) -> Void in
+            DispatchQueue.main.sync {
+                SVProgressHUD.dismiss()
+                
+                // Process Response
+                self.processResponseForDeleteRequest(list, recordID: recordID, error: error)
+            }
+        }
+    }
+    
+    private func processResponseForDeleteRequest(_ record: CKRecord, recordID: CKRecordID?, error: Error?) {
+        var message = ""
+        
+        if let error = error {
+            print(error)
+            message = "We are unable to delete the list."
+            
+        } else if recordID == nil {
+            message = "We are unable to delete the list."
+        }
+        
+        if message.isEmpty {
+            // Calculate Row Index
+            let index = self.lists.index(of: record)
+            
+            if let index = index {
+                // Update Data Source
+                self.lists.remove(at: index)
+                
+                if lists.count > 0 {
+                    // Update Table View
+                    self.tableView.deleteRows(at: [NSIndexPath(row: index, section: 0) as IndexPath], with: .right)
+                    
+                } else {
+                    // Update Message Label
+                    messageLabel.text = "No Records Found"
+                    
+                    // Update View
+                    updateView()
+                }
+            }
+            
+        } else {
+            // Initialize Alert Controller
+            let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            
+            // Present Alert Controller
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: -
+    // MARK: Segue Life Cycle
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
+        // Save Selection
+        selection = indexPath.row
+        
+        // Perform Segue
+        performSegue(withIdentifier: "ListDetail", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        // Fetch Destination View Controller
+        let addListViewController = segue.destination as! AddListViewController
+
+        // Configure View Controller
+        addListViewController.delegate = self
+
+        if let selection = selection {
+            // Fetch List
+            let list = lists[selection]
+
+            // Configure View Controller
+            addListViewController.list = list
+        }
+    }
+    
+}
+
